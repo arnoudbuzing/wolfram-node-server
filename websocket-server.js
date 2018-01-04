b158214d4c6de006b22997ab2d7b36a8711c4a2d
Arnoud Buzing <arnoudb@wolfram.com>
2018-01-04 13:53:19 -0600

    updates

--- websocket-server.js
+++ websocket-server.js
@@ -14,7 +14,7 @@ var wss = new wsServer({ server: httpsServer });
 
 wss.on('connection', function(socket) {
  console.log((new Date()) + ' Connection accepted.');
- var wolfram = require('child_process').spawn('c:\\Program Files\\Wolfram Research\\Mathematica\\11.3\\wolfram.exe');
+ var wolfram = require('child_process').spawn('c:\\Program Files\\Wolfram Research\\Mathematica\\11.2\\wolfram.exe');
  wolfram.stdout.on('data', function(data) { socket.send(String(data)); });
  socket.on('message', function(message) {
   socket.send(String(message));
